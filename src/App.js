a1fd8878337f6ebe820b03aec8de0d7d9783522e
Win Aung <91712429+winaung1@users.noreply.github.com>
2024-01-27 10:00:10 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='px-6 text-4xl font-bold tracking-widest\'>
+          We are going to deploy our website.
         </p>
+        <ul className='py-10 text-yellow-400'>
+          <li>Github</li>
+          <li>Tailwind</li>
+          <li>React</li>
+          <li>Vercel/Netlify</li>
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Are you ready like I am?
         </a>
       </header>
     </div>
